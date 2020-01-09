e3ba3a7b30319db0729ab64613a0e5c752379cf9
sjbeskur <sam.beskur@gmail.com>
2020-01-08 23:39:06 -0700

    formating

--- tests/tests.rs
+++ tests/tests.rs
@@ -6,6 +6,7 @@ use snmplib::SyncSession;
 #[test]
 fn it_works() {
 
-    let session = SyncSession::new("192.168.88.1", "ggc_ro".as_bytes(), Some(std::time::Duration::from_secs(2)), 0);
-    assert_eq!(2 + 2, 4);
+    let session = SyncSession::new("192.168.88.1", "public".as_bytes(), Some(std::time::Duration::from_secs(2)), 0);
+
+    assert_eq!(2 + 2, 4); // TODO add real tests
 }
